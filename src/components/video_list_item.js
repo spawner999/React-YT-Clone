babaaf3a68e6a45692659bf6c0ee3bf7ea18887f
spawner999 <lore.ferrario@gmail.com>
2016-05-03 11:27:47 -0700

    add video selection, remember that here you pass callback down and not actions up

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,8 +1,22 @@
 import React from 'react';
 
-const VideoListItem = (props) => {
-  return <li>video</li>;
-
+const VideoListItem = ({video, onVideoSelect}) => { //es6 syntax
+  // const video = props.video; does this
+  const imageUrl = video.snippet.thumbnails.default.url;
+  return (
+    <li onClick={() => onVideoSelect(video)} className="list-group-item">
+      <div className="video-list media">
+        <div className="media-left">
+          <img className="media-object" src={imageUrl} />
+        </div>
+        <div className="media-body">
+          <div className="media-heading">
+            {video.snippet.title}
+          </div>
+        </div>
+      </div>
+    </li>
+  );
 };
 
 export default VideoListItem;
